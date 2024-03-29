a8747a37f9d9cf165a32e23dafe8c5b65fc46634
Yashkrit-Singh <139995167+Yashkrit-Singh@users.noreply.github.com>
2024-03-29 08:53:14 +0530

    Gallery DOne

--- Startup-Incubator-Page/src/Gallery/Gallery.jsx
+++ Startup-Incubator-Page/src/Gallery/Gallery.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import PhotoGrid from './PhotoGrid';
-import Lightgallery from '../Components/Lightgallery';
-import data from "./GalleryPhoto.json";
+// Gallery.js
+import React, { useState } from "react";
+import PhotoItem from "./PhotoItem"; // Import PhotoItem component
+import photos from "./GalleryPhoto.json";
+import "./Gallery.css";
 
 const Gallery = () => {
+    const [photo, setPhoto] = useState(null);
     return (
         <div className="container mx-auto">
-            <h1 className="text-5xl font-bold text-center my-4">Photo Gallery</h1>
-            <Lightgallery speed = {500}>
-                <PhotoGrid photos = {data} />
-            </Lightgallery>
+            <h1 className="text-6xl font-medium text-orange-600 text-center my-10">Gallery of VOH Incubator</h1>
+            <div className="flex flex-wrap justify-center">
+                {photos.map((photo) => (
+                    <div
+                        key={photo.id}
+                        className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3"
+                        onClick={() => setPhoto(photo)}
+                    >
+                        <PhotoItem photo={photo} />
+                    </div>
+                ))}
+            </div>
+
+            <div className="popup-media" style={{display: photo ? 'block' : 'none'}}>
+                <span onClick={() => setPhoto(null)}>&times;</span>
+                <div>
+                    <img src={photo?.imgUrl} alt="" />
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Gallery;
--- Startup-Incubator-Page/src/Gallery/PhotoItem.jsx
+++ Startup-Incubator-Page/src/Gallery/PhotoItem.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 
 function PhotoItem({ photo }) {
     return (
-        <div className="border border-gray-300 p-4 m-2">
-            <a 
-                href={photo.imgUrl} 
-                className="block transition-transform duration-300 ease-in-out transform hover:scale-105"
-            >
+        <div className="border border-gray-300 p-4 m-2 block transition-transform duration-300 ease-in-out transform hover:scale-105">
                 <img src={photo.imgUrl} className="w-full" />
-            </a>
         </div>
     );
 }
